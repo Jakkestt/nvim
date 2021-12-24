085ccf19bcaeb658e925f2bc5528fcb7f1670dc4
Jarkko Arffman <41551796+Jakkestt@users.noreply.github.com>
2021-12-24 10:25:28 +0200

    Disable telemetry

--- lua/user/lsp/settings/sumneko_lua.lua
+++ lua/user/lsp/settings/sumneko_lua.lua
@@ -5,11 +5,17 @@ return {
 			diagnostics = {
 				globals = { "vim" },
 			},
+
 			workspace = {
 				library = {
 					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
 					[vim.fn.stdpath("config") .. "/lua"] = true,
+					[vim.fn.expand("/home/jarkko/Games/World of Warcraft/_retail_/BlizzardInterfaceCode/Interface")] = true,
 				},
+			},
+
+			telemetry = {
+				enable = false,
 			},
 		},
 	},
