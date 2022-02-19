e3ab85d1dc984daa64ab88baa213d4af24e557c0
Jarkko Arffman <41551796+Jakkestt@users.noreply.github.com>
2022-02-19 15:49:23 +0200

    commit

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -94,9 +94,6 @@ return packer.startup(function(use)
 	-- Git
 	use("lewis6991/gitsigns.nvim")
 
-	-- Prettier
-	use("MunifTanjim/prettier.nvim")
-
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
